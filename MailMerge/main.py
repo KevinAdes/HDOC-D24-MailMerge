99f767d90ff2546309aad473d28d2b47e9941bc4
KevinAdes <kra18@hampshire.edu>
2024-06-29 19:56:59 -0400

    Commit
    Simple project for learning how to read write and create files in pythons. Using a list of the straw hat pirates (except chopper) and a birthday invitation template, generates a series of invitations for each pirate (except chopper)

---
+++ MailMerge/main.py
@@ -0,0 +1,12 @@
+﻿names = []
+
+with open("./Input/Names/invited_names.txt", encoding='utf-8-sig') as invited_names:
+    for name in invited_names.readlines():
+        names.append(name.removesuffix("\n"))
+
+
+with open("./Input/Letters/starting_letter.txt", encoding='utf-8-sig') as starting_letter:
+    lettercopy = starting_letter.read()
+    for invitee in names:
+        with open(f"./Output/ReadyToSend/LetterFor{invitee}", mode="x") as invitation:
+            invitation.write(lettercopy.replace("[NAME]", invitee))
